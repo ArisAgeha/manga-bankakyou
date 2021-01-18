6fcfda776d742b768c27a3e6caf2a490f6d05225
黄沛 <87725092@qq.com>
2021-01-18 16:01:39 +0800

    update README.md && update galleryConfig interface

--- src/database/entity/configuration.dto.ts
+++ src/database/entity/configuration.dto.ts
@@ -15,11 +15,25 @@ export interface WorkbenchConfig {
     show_sidebar: boolean;
 }
 
+export type PreviewMode = 'waterfall' | 'default';
+export type ReadMode = 'single' | 'double' | 'scroll';
+
 export interface GalleryConfig {
-    default_view_mode: 'double';
-    default_pics_per_line: number;
+    default_preview_mode: PreviewMode;
+    default_read_mode: ReadMode;
+
+    /** waterfall */
+    waterfall_pics_per_line: number;
+
+    /** single */
+    single_direction: 'LR' | 'RL';
+
+    /** double */
     double_direction: 'LR' | 'RL';
+
+    /** scroll */
     scroll_direction: 'TB' | 'BT' | 'LR' | 'RL';
+    scroll_picture_per_page: number;
 }
 
 export type Configs = {
